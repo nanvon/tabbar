81ad4e2ebda296861503d43227d5167b34cf4cb7
南枫 <nanvon@outlook.com>
2021-09-26 22:30:08 +0800

    init repo

---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tab-bar-item">
+    <slot v-if="!isActive" name="item-icon"></slot>
+    <slot v-else name="item-icon-active"></slot>
+    <div :class="{active:isActive}">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  data() {
+    return {
+      isActive: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+@import "../../assets/css/base.css";
+
+.tab-bar-item {
+  height: 49px;
+  flex: 1;
+  text-align: center;
+  font-size: 14px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+
+.active {
+  color: #ff5777;
+}
+</style>
