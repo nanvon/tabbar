463202c2773a3939ff59fbf87ea47397846760c7
南枫 <nanvon@outlook.com>
2021-09-30 20:51:15 +0800

    axios 拦截器

--- src/network/request.js
+++ src/network/request.js
@@ -26,6 +26,30 @@ export function request(config) {
     timeout: 5000
   });
 
+  // 2、axios拦截器
+  instance.interceptors.request.use(
+    config => {
+      // console.log(config);
+      // 1、比如config中的一些信息不符合服务器的要求
+      // 2、比如每次发送网络请求时，都希望在界面中显示一个请求的loading动画
+      // 3、某些网络请求（比如登录（token）），必须携带一些特殊的信息
+      return config;
+    },
+    err => {
+      // console.log(err);
+    }
+  );
+
+  instance.interceptors.response.use(
+    res => {
+      // console.log(res);
+      return res.data;
+    },
+    err => {
+      // console.log(err);
+    }
+  );
+
   // 发送真正的网络请求
   return instance(config);
 }
